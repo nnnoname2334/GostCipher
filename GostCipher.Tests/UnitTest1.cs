28823edf32180bcf0bc3cdea1973f77ec62c8937
234989 <234989@KB37-120-C10>
2026-05-08 13:44:07 +0300

    добавил тесты

---
+++ GostCipher.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GostCipher.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
